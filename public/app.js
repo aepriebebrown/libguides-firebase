fac365144029512cf4a4846acb8d200a2a5bc078
aepriebebrown <aepriebe@gmail.com>
2018-09-09 10:41:42 -0500

    Cleaned up code, grouping variables together when possible, removing unnecessary comments and logs, formatting line indentation, and adding comments where needed.

--- public/app.js
+++ public/app.js
@@ -10,77 +10,68 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const dbRefObject = firebase.database().ref().child('favorites');
 
-    /*dbRefObject.on('value', snap => {
-        console.log(snap.val());
-        myFavorites.innerText = JSON.stringify(snap.val(), null, 3);*/
-    });
-
-//});
+});
 
 var welcomeMessage = document.getElementById("welcome-message");
 var signInButtonElement = document.getElementById('sign-in');
 var signOutButtonElement = document.getElementById('sign-out');
 var userNameElement = document.getElementById('user-name');
 
+//Allow user to sign in
 function googleLogin() {
     var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
 
-            .then(result => {
-                const user = result.user;
-                welcomeMessage.innerHTML = `Hello ${user.displayName}`;
-                console.log("1", user)
-            })
-            .catch(console.log);
-            //initFirebaseAuth();
+    .then(result => {
+        const user = result.user;
+        welcomeMessage.innerHTML = `Hello ${user.displayName}`;
+    })
+    .catch(console.log);
 }
 
 function signOut() {
-  firebase.auth().signOut();
-  console.log("signed out");
-  welcomeMessage.innerHTML = "You are signed out.";
+    firebase.auth().signOut();
+    console.log("signed out");
+    welcomeMessage.innerHTML = "You are signed out.";
 }
-
-/*function initFirebaseAuth() {
-  firebase.auth().onAuthStateChanged(authStateObserver);
-}*/
 
 function initFirebaseAuth() {
 
     firebase.auth().onAuthStateChanged(function(user) {
-    console.log('user', user);
+        console.log('user', user);
 
-    if (user) { // User is signed in!
-      // Get the signed-in user's profile pic and name.
-      var userName = getUserName();
+        if (user) { // User is signed in!
+            // Get the signed-in user's name.
+            var userName = getUserName();
 
-      // Set the user's name.
-      userNameElement.textContent = "Logged in as " + userName;
+            // Set the user's name.
+            userNameElement.textContent = "Logged in as " + userName;
 
-      // Show user's profile and sign-out button.
-      userNameElement.removeAttribute('hidden');
-      signOutButtonElement.removeAttribute('hidden');
+            // Show user's profile and sign-out button.
+            userNameElement.removeAttribute('hidden');
+            signOutButtonElement.removeAttribute('hidden');
 
-      // Hide sign-in button.
-      signInButtonElement.setAttribute('hidden', 'true');
+            // Hide sign-in button.
+            signInButtonElement.setAttribute('hidden', 'true');
 
-    } else { // User is signed out!
-      // Hide user's profile and sign-out button.
-      userNameElement.setAttribute('hidden', 'true');
-      signOutButtonElement.setAttribute('hidden', 'true');
+        } else { // User is signed out!
+            // Hide user's profile and sign-out button.
+            userNameElement.setAttribute('hidden', 'true');
+            signOutButtonElement.setAttribute('hidden', 'true');
 
-      // Show sign-in button.
-      signInButtonElement.removeAttribute('hidden');
-    }
-})
+            // Show sign-in button.
+            signInButtonElement.removeAttribute('hidden');
+        }
+    })
 }
 
+//Check if user is signed in
 function isUserSignedIn() {
-  return !!firebase.auth().currentUser;
-  console.log('user', user);
+    return !!firebase.auth().currentUser;
 }
 
+//I feel like this is self-explanatory, but this gets the username
 function getUserName() {
-  return firebase.auth().currentUser.displayName;
+    return firebase.auth().currentUser.displayName;
 }
--- public/libguides.js
+++ public/libguides.js
@@ -1,71 +1,76 @@
 var apiUrl = "https://lgapi-us.libapps.com/1.1/guides?site_id=8488&key=0b8da796b00334ae3471f60e6a10e8c6&search_terms=";
-    var input = document.querySelector(".libguides-input");
-    var libguidesName = document.querySelector(".libguides-name");
-    var libguidesImage = document.querySelector(".libguides-image");
-    var favoriteList = document.getElementById("favorites");
+var input = document.querySelector(".libguides-input");
+var libguidesName = document.querySelector(".libguides-name");
+var libguidesImage = document.querySelector(".libguides-image");
+var favoriteList = document.getElementById("favorites");
 
-    function getLibguidesData() {
-        //Use Axios to GET data
-        axios.get(apiUrl + input.value)
-        //Display the data in the DOM
-        .then(function (response) {
-            //Clear the list contents
-            libguidesName.innerHTML = '';
-            console.log("Working");
-            console.log(response.data.length);
-            //Make an array to store multiple results
-            var namesList = [];
-            var descriptionList = [];
-            var urlList = [];
-            var idList = [];
-            //Loop over each response
-            for (var i = 0; i < response.data.length; i++) {
-                   var nameOfLibguide = response.data[i].name;
-                      console.log(nameOfLibguide);
-                      //Add data from each result to the namesList array
-                      namesList.push(nameOfLibguide);
-                      console.log(namesList);
-                      var descriptionOfLibguide = response.data[i].description;
-                      console.log(descriptionOfLibguide);
-                      descriptionList.push(descriptionOfLibguide);
-                      var urlOfLibguide = response.data[i].url;
-                      console.log(urlOfLibguide);
-                      var idOfLibguide = response.data[i].id;
-                      idList.push(idOfLibguide);
-                      urlList.push(urlOfLibguide);
-                      //Create an li for each response
-                      var listItem = document.createElement('li');
-                      listItem.setAttribute("id", idList[i]);
+function getLibguidesData() {
+    //Use Axios to GET data
+    axios.get(apiUrl + input.value)
 
-                      // Add the item text
-                      listItem.innerHTML = '<a href="' + urlList[i] + '" target="_blank">' + namesList[i] + '</a><br><br>' + descriptionList[i] +'<br><br><button class="' + idList[i] + '" onClick="favButton(this.parentNode.id)">Favorite</button>';
+    //Display the data in the DOM
+    .then(function (response) {
 
-                      // Add listItem to the list
-                      libguidesName.appendChild(listItem);
-                  }
-            //If no results, display a message in the DOM
-            if (namesList.length <= 0) {
-              console.log("No Results");
-              libguidesName.innerHTML = "No results.";
-            }
-        })
+        //Clear the list contents
+        libguidesName.innerHTML = '';
 
-        //Error catching
-        .catch(function (error) {
-            libguidesName.innerHTML = "(An error has occurred.)";
-            console.log("Error!");
-        });
-    }
+        //Make an array to store multiple results
+        var namesList = [];
+        var descriptionList = [];
+        var urlList = [];
+        var idList = [];
 
-    function favButton(clicked_id) {
-        var id = clicked_id;
-        var buttonClass = String(id);
-        var favoriteItem = document.createElement('li');
-        console.log("li created");
-        favoriteItem.innerHTML = document.getElementById(clicked_id).innerHTML;
-        favoriteList.appendChild(favoriteItem);
-    }
+        //Loop over each response
+        for (var i = 0; i < response.data.length; i++) {
+            var nameOfLibguide = response.data[i].name;
+            var descriptionOfLibguide = response.data[i].description;
+            var urlOfLibguide = response.data[i].url;
+            var idOfLibguide = response.data[i].id;
+            var listItem = document.createElement('li');
 
-    //Find button and listen for a click event
-    var button = document.querySelector(".libguides-button");
-    button.addEventListener("click", getLibguidesData);
+            //Add data from each result to the namesList array
+            namesList.push(nameOfLibguide);
+            descriptionList.push(descriptionOfLibguide);
+            idList.push(idOfLibguide);
+            urlList.push(urlOfLibguide);
+
+            //Create an li for each response
+            listItem.setAttribute("id", idList[i]);
+
+            // Add the item text
+            listItem.innerHTML = '<a href="' + urlList[i] + '" target="_blank">' + namesList[i] + '</a><br><br>' + descriptionList[i] +'<br><br><button class="' + idList[i] + '" onClick="favButton(this.parentNode.id)">Favorite</button>';
+
+            // Add listItem to the list
+            libguidesName.appendChild(listItem);
+        }
+
+        //If no results, display a message in the DOM
+        if (namesList.length <= 0) {
+            console.log("No Results");
+            libguidesName.innerHTML = "No results.";
+        }
+    })
+
+    //Error catching
+    .catch(function (error) {
+        libguidesName.innerHTML = "(An error has occurred.)";
+        console.log("Error!");
+    });
+}
+
+//Allow user to add to Favorites list
+function favButton(clicked_id) {
+    var id = clicked_id;
+    var buttonClass = String(id);
+    var favoriteItem = document.createElement('li');
+
+    //Display record from search results inside favorites li
+    favoriteItem.innerHTML = document.getElementById(clicked_id).innerHTML;
+
+    //Add the item to the favorites list
+    favoriteList.appendChild(favoriteItem);
+}
+
+//Find button and listen for a click event
+var button = document.querySelector(".libguides-button");
+button.addEventListener("click", getLibguidesData);
